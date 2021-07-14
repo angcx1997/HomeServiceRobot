b013a24d3042c936a3849f06707e7076f8e4e03b
angcx1997 <ang.chin.xian@gmail.com>
2021-07-14 15:55:57 +0800

    Localization and Navigation Testing - deploy ros navigation stack to localize robot with map created using gmapping - noted that the localization can only if changing the local costmap odom to map

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -2,7 +2,7 @@
 xterm  -e  "export TURTLEBOT3_MODEL=burger; roslaunch turtlebot3_gazebo turtlebot3_myworld.launch" &
 sleep 5
 xterm  -e  "roslaunch turtlebot3_gazebo turtlebot3_myworld_rviz.launch" &
+# sleep 5
+# xterm  -e  "export TURTLEBOT3_MODEL=burger; roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch" &
 sleep 5
-xterm  -e  "export TURTLEBOT3_MODEL=burger; roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch" &
-sleep 5
-xterm -e "roslaunch gmapping gmapping_demo.launch"
+xterm -e "roslaunch turtlebot3_navigation amcl_demo.launch"
